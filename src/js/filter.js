9ece8a10976249d2656822dcb70bbc98264ed1e7
Azamat Abduvohidov <creativeaah@gmail.com>
2023-06-19 16:57:46 +0500

    countries app completed with webpack

---
+++ src/js/filter.js
@@ -0,0 +1,54 @@
+// const searchFormEl = document.querySelector('.search')
+
+// searchFormEl.search.addEventListener("input", () => {
+//     const searchValue = searchFormEl.search.value.toLowerCase()
+//     const cardsItem = document.querySelectorAll('.cards__item')
+//     const cardsTitle = document.querySelectorAll('.cards__title')
+//     cardsTitle.forEach((title, i) => {
+//         if(title.textContent.toLowerCase().includes(searchValue)) {
+//             cardsItem[i].style.display = 'none'
+//         } else {
+//             cardsItem[i].style.color = 'red'
+//         }
+//     })
+// })
+const searchFormEl = document.querySelector('.search')
+
+searchFormEl.search.addEventListener("input", () => {
+    const searchValue = searchFormEl.search.value.toLowerCase()
+    const cardsItem = document.querySelectorAll('.cards__item')
+    const cardsTitles = document.querySelectorAll('.cards__title')
+    console.log(cardsTitles)
+    cardsTitles.forEach((title, i) => {
+        if(title.textContent.toLowerCase().includes(searchValue)) {
+            cardsItem[i].style.display = 'block'
+        } else {
+            cardsItem[i].style.display = 'none'
+        }
+    })
+})
+
+const searchSelect = document.querySelectorAll('.search__select-list li')
+
+const searchSelectSpan = document.querySelector('.search__select span')
+
+import { createCountries } from "./updateUI"
+import request from "./request"
+
+searchSelect.forEach((li) => {
+    li.addEventListener('click', () => {
+        searchSelectSpan.textContent = li.textContent
+        let filterAPI
+        if(li.textContent == 'All') {
+            filterAPI = 'https://restcountries.com/v3.1/all'
+        } else {
+            filterAPI = `https://restcountries.com/v3.1/region/${li.textContent}`
+        }
+
+        request(filterAPI).then((data) => {
+            createCountries(data)
+        }).catch((error) => {
+            console.error(error)
+        })
+    })
+})
